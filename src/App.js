84484ed77d2415aefbbbf78010b791a6d4692ad4
Evan Chan <evanchan@MacBook-Pro.local>
2022-10-29 01:05:34 -0400

    Initial commit of create-react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './duck.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello im not evan chan
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
